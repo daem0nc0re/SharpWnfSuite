454e794d4adcc22caac634c166f6827d4831b8dc
daem0nc0re <daem0nc0re@outlook.com>
2025-04-08 11:13:48 +0900

    Coding manner fix

--- SharpWnfSuite/SharpWnfDump/Library/Globals.cs
+++ SharpWnfSuite/SharpWnfDump/Library/Globals.cs
@@ -4,9 +4,9 @@
     {
         public static string[] LifetimeKeyNames = new string[]
         {
-            "SYSTEM\\CurrentControlSet\\Control\\Notifications",
-            "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Notifications",
-            "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\VolatileNotifications"
+            @"SYSTEM\CurrentControlSet\Control\Notifications",
+            @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Notifications",
+            @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\VolatileNotifications"
         };
     }
 }
